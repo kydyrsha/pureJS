33824e7a324a385fc7a600b2f2c3e426281e87d8
kydyrsha <sanatkydyrsha@gmail.com>
2020-06-23 16:18:42 +0600

    numbers in javascript

---
+++ numbers.js
@@ -0,0 +1,30 @@
+// Number
+//
+// const num = 42 //integer
+// const float = 42.42 //float
+// const pow = 10e3 //
+//
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Math.pow(2,53) - 1)
+// console.log(Number.MIN_SAFE_INTEGER)
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE);
+// console.log(Number.POSITIVE_INFINITY)
+// console.log(Number.NEGATIVE_INFINITY)
+// console.log(Number.NaN)
+// console.log(typeof NaN)
+// console.log(isFinite(Infinity)) // Number.isFinite()
+// console.log(isFinite(11))
+//
+// const stringInt = '42'
+// const stringFloat = '42.42'
+// console.log(Number.parseInt(stringInt) + 2)
+// console.log(Number(stringInt) + 2)
+// console.log(+stringInt + 2)
+//
+// console.log(parseFloat(stringInt) + 2); // anology
+
+
+// console.log(0.4 + 0.2) // 0.6
+// console.log((0.4 + 0.2).toFixed(1)) // string
+// console.log(+(0.4 + 0.2).toFixed(1)) // number
