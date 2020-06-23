947db7e1e03406078e1b851c562887d0594ca632
kydyrsha <sanatkydyrsha@gmail.com>
2020-06-23 13:03:01 +0600

    some basics operators and functions so far (variables and some other things are skipped)

---
+++ main.js
@@ -0,0 +1,60 @@
+// // // Операторы
+// // const courseStatus = 'fail' // ready, fail, pending
+// //
+// // if (courseStatus === 'ready') {
+// //   console.log('Курс уже готов и его можно проходить')
+// // } else if (courseStatus === 'pending') {
+// //   console.log('Курс пока находится в процессе разработки')
+// // } else {
+// //   console.log('Курс брлбрлм...сломано')
+// // }
+// //
+// const isReady = true
+// //
+// // if (isReady) {
+// //   console.log('I am ready')
+// // } else {
+// //   console.log('I am not ready')
+// // }
+//
+// // Тернарный оператор
+// isReady ? console.log('I am ready') : console.log('Vse')
+
+
+// Логические операторы
+
+// Оператор И
+// true && true --> true
+// false && true --> false
+// true && false --> false
+// false && false --> false
+
+// Оператор ИЛИ
+// true || true --> true
+// false || true --> true
+// true || false --> true
+// false || false --> false
+
+
+// Функции
+
+// function calculateAge(year) {
+//   return 2020 - year
+// }
+//
+// // console.log(calculateAge(1993))
+// // console.log(calculateAge(1997))
+// // console.log(calculateAge(1990))
+//
+// function logInfoAbout (name, year) {
+//   const age = calculateAge(year)
+//
+//   if (age > 0) {
+//     console.log('Человек по имени ' + name + ' takogo возрастa ' + age)
+//   } else {
+//     console.log('Voobwe-to ewe net' + age)
+//   }
+//
+// }
+//
+// logInfoAbout('Sanat',1997)
