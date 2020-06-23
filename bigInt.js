ad5d0cb58ea5528f329aa6fc186a6b0ebf3c7452
kydyrsha <sanatkydyrsha@gmail.com>
2020-06-23 16:32:17 +0600

    bigInt and Math operations

---
+++ bigInt.js
@@ -0,0 +1,33 @@
+// bigInt
+// Для работы с большими числами
+
+// console.log(typeof 9007199254740991n)
+// console.log(typeof 9007199254740991n - 9007199254740991n)
+// console.log(-9007199254740991n)
+// console.log(9007199254740991.2333n) // error cannot work with other types
+// console.log(10n - 4) // error
+// console.log(10n - BigInt(2))
+
+// Math
+
+// console.log(Math.E)
+// console.log(Math.PI)
+//
+// console.log(Math.sqrt(25))
+// console.log(Math.pow(3,3)) // degree
+// console.log(Math.abs(-32)) // module
+// console.log(Math.max(11,3,44,55,12)) // chooses the max
+// console.log(Math.min(11,3,44,55,12)) // chooses the min
+// console.log(Math.floor(4.9)) // to the lowest
+// console.log(Math.ceil(4.9)) // to the highest
+// console.log(Math.round(4.9)) // to the closest int
+// console.log(Math.random()) // random number
+//
+//
+// // Example
+//
+// function getRandomBetween(min,max) {
+//   return Math.floor(Math.random() * (max-min) + min)
+// }
+//
+// console.log(getRandomBetween(10,42))
